aa7ce2a905b58d2f15fe47b753932da062fc3062
Peeratach <76504724+Peeratach@users.noreply.github.com>
2021-02-10 10:45:19 +0700

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,12 @@
+<?php
+    $conn = mysqli_connect("localhost", "root", "", "movie");
+    $conn->set_charset("utf8");
+    
+    
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+    } else {
+        //echo "Movie ได้ถูกใช้งาน";
+    }
+?>
